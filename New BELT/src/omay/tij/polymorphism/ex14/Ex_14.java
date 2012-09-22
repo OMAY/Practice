008843a913bf0022ffd83f2cf11a9016151162b4
OMAY <mayvaldoff@gmail.com>
2012-09-22 13:12:31 +0300

    14

---
+++ New BELT/src/omay/tij/polymorphism/ex14/Ex_14.java
@@ -0,0 +1,60 @@
+package omay.tij.polymorphism.ex14;
+
+/*Modify Exercise 12 so that one of the member objects is 
+ a shared object with reference counting, and demonstrate that it works 
+ properly.  */
+class Shared {
+	private long refcount;
+
+	public void addRef() {
+		refcount++;
+		System.out.println(refcount);
+	}
+
+}
+
+class Rodent {
+	private Shared s;
+
+	public Rodent(Shared sh) {
+		System.out.print("New Rodent ");
+		s = sh;
+		s.addRef();
+	}
+
+}
+
+class Mouse extends Rodent {
+	public Mouse(Shared sh) {
+		super(sh);
+		System.out.println("New Mouse ");
+	}
+
+}
+
+class Gerbil extends Rodent {
+	public Gerbil(Shared sh) {
+		super(sh);
+		System.out.println("New Gerbil ");
+	}
+
+}
+
+class Hamster extends Rodent {
+	public Hamster(Shared sh) {
+		super(sh);
+		System.out.println("New Hamster ");
+	}
+
+}
+
+public class Ex_14 {
+	public static void main(String[] args) {
+		Shared shared = new Shared();
+		new Mouse(shared);
+		new Gerbil(shared);
+		new Hamster(shared);
+
+	}
+
+}
