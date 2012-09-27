1f3c3c9c19d28a8d6466e0c966d6096eed9ebf3a
OMAY <mayvaldoff@gmail.com>
2012-09-27 20:55:18 +0300

    17-19

---
+++ New BELT/src/omay/tij/ch_10_interfaces/ex18/Ex_18.java
@@ -0,0 +1,56 @@
+package omay.tij.ch_10_interfaces.ex18;
+
+/*Create a Cycle interface, with implementations
+Unicycle, Bicycle and Tricycle. Create factories for each type of Cycle,
+and code that uses these factories.
+ */
+interface Cycle{
+    void move();
+
+}
+interface CycleFactory{
+    Cycle ride();
+}
+class Unicycle implements Cycle{
+    public void move(){
+        System.out.println("Ubicycle moving");
+    }
+}
+class UnicycleFactory implements CycleFactory{
+    public Cycle ride(){
+        return new Unicycle();
+
+    }
+}
+class Bicycle implements Cycle{
+    public void move(){
+        System.out.println("Bicycle moving");
+    }
+}
+class BicycleFactory implements CycleFactory{
+    public Cycle ride(){
+        return new Bicycle();
+    }
+}
+class Tricycle implements Cycle{
+    public void move(){
+        System.out.println("Tricycle moving");
+    }
+}
+class TricycleFactory implements CycleFactory{
+    public Cycle ride(){
+        return new Tricycle();
+    }
+}
+
+public class Ex_18 {
+    public static void ridding(CycleFactory cycleFactory){
+        Cycle c = cycleFactory.ride();
+        c.move();
+    }
+    public static void main(String[]args){
+        ridding(new UnicycleFactory());
+        ridding(new BicycleFactory());
+        ridding(new TricycleFactory());
+    }
+}
