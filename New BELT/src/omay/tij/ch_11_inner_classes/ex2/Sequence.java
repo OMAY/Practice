9539356411af9bbaa770604936654fc9e46e94f8
OMAY <mayvaldoff@gmail.com>
2012-09-28 20:53:53 +0300

    chapter 11 ex 1-5

---
+++ New BELT/src/omay/tij/ch_11_inner_classes/ex2/Sequence.java
@@ -0,0 +1,45 @@
+package omay.tij.ch_11_inner_classes.ex2;
+/*Create a class that holds a String, and has a toString ()
+ method that displays this String. Add several instances of your new class to a
+Sequence object, then display them. */
+interface Selector{
+    boolean end();
+    Object current();
+    void next();
+}
+public class Sequence{
+    private Object[]items;
+    private int next=0;
+    public Sequence(int size){
+        items=new Object[size];
+    }
+    public void add(Object x){
+        if(next<items.length)
+            items[next++]=x;
+    }
+    private class SequenceSelector implements Selector{
+        private int i=0;
+        public boolean end(){
+            return i==items.length;
+        }
+        public Object current(){
+            return items[i];
+        }
+        public void next(){
+            if( i<items.length)i++;
+        }
+    }
+    public Selector selector(){
+        return new SequenceSelector();
+    }
+    public static void main(String[]args){
+        Sequence sequence=new Sequence(10);
+        for(int i=0 ;i<10;i++)
+            sequence.add(new StringHolder());
+        Selector selector=sequence.selector();
+        while(!selector.end()){
+            System.out.print(selector.current()+" ") ;
+            selector.next();
+        }
+    }
+}
---
+++ New BELT/src/omay/tij/ch_11_inner_classes/ex5/Outer.java
@@ -0,0 +1,13 @@
+package omay.tij.ch_11_inner_classes.ex5;
+
+public class Outer {
+    public Outer(){
+        System.out.println("A new Outer class");
+    }
+    class Inner{
+        public Inner(){
+            System.out.println("A new Inner class");
+        }
+
+    }
+}
